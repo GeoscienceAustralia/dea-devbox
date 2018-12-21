33da4cbf2db85e31c91fc788fbd563b3028b8ae5
Kirill Kouzoubov <kirill888@gmail.com>
2018-12-21 14:17:40 +1100

    Pass through AWS_DEFAULT_REGION in aws_creds as well
    Also switching to botocore from boto3 to reduce dependency tree

--- docker/aws_creds.py
+++ docker/aws_creds.py
@@ -1,15 +1,25 @@
-import boto3
+import botocore.session
 
-session = boto3.Session()
-creds = session.get_credentials().get_frozen_credentials()
 
-env_mapping = dict(
-    AWS_ACCESS_KEY_ID='access_key',
-    AWS_SECRET_ACCESS_KEY='secret_key',
-    AWS_SESSION_TOKEN='token')
+def main():
+    session = botocore.session.get_session()
+    creds = session.get_credentials().get_frozen_credentials()
 
-for e, p in env_mapping.items():
-    if hasattr(creds, p):
-        val = getattr(creds, p)
-        if val is not None:
-            print("export {e}='{val}'".format(e=e, val=val))
+    env_mapping = dict(
+        AWS_ACCESS_KEY_ID='access_key',
+        AWS_SECRET_ACCESS_KEY='secret_key',
+        AWS_SESSION_TOKEN='token')
+
+    for e, p in env_mapping.items():
+        if hasattr(creds, p):
+            val = getattr(creds, p)
+            if val is not None:
+                print("export {e}='{val}'".format(e=e, val=val))
+
+    region = session.get_config_variable('region')
+    if region is not None:
+        print("export AWS_DEFAULT_REGION='{}'".format(region))
+
+
+if __name__ == '__main__':
+    main()
